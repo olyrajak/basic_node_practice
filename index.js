ccedc17b4a081f3d7e80fc39c5a8bac13bb1fcc3
Oly Rajak <59551153+olyrajak@users.noreply.github.com>
2022-10-08 09:57:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var path = require('path');
 var freeM = os.freemem();
 var cpu = os.cpus().length;
 var fs = require('fs');
+var url = require('url');
 var filepath = path.join(__dirname, 'file.md');
 console.log(freeM, cpu);
 fs.readFile('file.md', (err, content) => {
@@ -24,3 +25,8 @@ console.log(buff1.toString());
 var math = require('./math');
 console.log(math.sum(3, 5));
 console.log(math.square(3));
+
+var urlstring = "https://altcampus.school/students/register?verified=false";
+
+var parseUrl = url.parse(urlstring);
+console.log(parseUrl);
