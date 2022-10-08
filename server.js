227f497b779c02a4d0ee8f30367f0e40129b6eef
Oly Rajak <olyrajak@gmail.com>
2022-10-09 00:36:36 +0530

    some update

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ var http = require('http');
 var server = http.createServer(handlerRequest);
 
 function handlerRequest(req, res) {
+    // console.log(req.method, req.url);
+    console.log(req.headers);
     res.end("Welcome Node");
 }
-server.listen(3000, () => {
-    console.log("Server is listen on port 3000");
+server.listen(6000, () => {
+    console.log("Server is listen on port 5000");
 });
