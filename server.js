2cc416bf82cb9120a0d59d42bbfe71398652e2f8
Oly Rajak <olyrajak@gmail.com>
2022-10-09 16:24:44 +0530

    Update Practice

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 var http = require('http');
-// http.createServer((request, response) => {
-//     console.log(request, response);
-//     response.end("Welcome");cc
-// }).listen(3000, 'localhost');
+http.createServer((request, response) => {
+    console.log(request, response);
+    // response.StatusCode = 200;
+    // response.setHeader = ('content-type', 'text/html');
+    response.writeHead(200, { 'content-type': 'text/html' });
+    response.write("Hello");
+    response.write("Hi");
+    response.end("<h1>Welcome</h1>");
+    // cc
+}).listen(3000, 'localhost');
 
-var server = http.createServer(handlerRequest);
+// var server = http.createServer(handlerRequest);
 
-function handlerRequest(req, res) {
-    // console.log(req.method, req.url);
-    console.log(req.headers);
-    res.end("Welcome Node");
-}
-server.listen(6000, () => {
-    console.log("Server is listen on port 5000");
-});
+// function handlerRequest(req, res) {
+//     // console.log(req.method, req.url);
+//     res.StatusCode = 200;
+//     console.log(req.headers);
+//     res.end("Welcome Node");
+// }
+// server.listen(900, () => {
+//     console.log("Server is listen on port 900");
+// });
