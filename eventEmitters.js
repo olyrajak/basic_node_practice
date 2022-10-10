e43e84e87a47476fcc7e2f34e80a8aed601893a2
Oly Rajak <olyrajak@gmail.com>
2022-10-10 21:53:14 +0530

    Practice Capture incoming data from request body
    Capturing data from request body

--- eventEmitters.js
+++ eventEmitters.js
@@ -1,11 +1,33 @@
-var { EventEmitter } = require('events');
+// var { EventEmitter } = require('events');
+var http = require('http');
+// var url = require('url');
+// var fs = require('fs');
+// var myEmitter = new EventEmitter();
+// myEmitter.on('notice', (msg) => {
+//     console.log("Event Emited" + msg);
+// });
+// myEmitter.on('notice', () => {
+//     console.log("Event Emited 2");
+// });
+// myEmitter.emit('notice', 'Hello World');
+// myEmitter.emit('notice');
 
-var myEmitter = new EventEmitter();
-myEmitter.on('notice', (msg) => {
-    console.log("Event Emited" + msg);
+
+
+
+
+var server = http.createServer(handlerRequest);
+
+function handlerRequest(req, res) {
+    var store = '';
+    req.on('data', (chunk) => {
+        store = store + chunk;
+    });
+    req.on('end', () => {
+        console.log(store);
+
+    });
+}
+server.listen(3333, () => {
+    console.log("Server is listen on port 3333");
 });
-myEmitter.on('notice', () => {
-    console.log("Event Emited 2");
-});
-myEmitter.emit('notice', 'Hello World');
-myEmitter.emit('notice');
