68f8f9d63cbac4c0681d2e2b34a4773d5b5eca2e
Oly Rajak <59551153+olyrajak@users.noreply.github.com>
2022-10-12 22:07:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,29 +5,46 @@ var cpu = os.cpus().length;
 var fs = require('fs');
 var url = require('url');
 var filepath = path.join(__dirname, 'file.md');
+
 console.log(freeM, cpu);
+
 fs.readFile('file.md', (err, content) => {
+    
     console.log(err, content.toString);
+    
 });
+
 fs.readFile('file.md', (err, msg) => {
+    
     console.log(err, msg.toString);
+    
 });
 let fileData = fs.readFileSync("file.md");
+
 console.log(fileData);
 
 let buff1 = Buffer.alloc(10);
+
 console.log(buff1);
 
 buff1.write("Welcome to Buffer");
+
 console.log(buff1);
 
 console.log(buff1.toString());
 var math = require('./math');
+
 console.log(math.sum(3, 5));
 console.log(math.square(3));
 
+
+
 var urlstring = "https://altcampus.school/students/register?verified=false";
 
+
+
 var parseUrl = url.parse(urlstring);
+
+
 
 console.log(parseUrl);
